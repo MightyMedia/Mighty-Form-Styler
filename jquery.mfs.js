602018f8591aeda318859ec3fb7cce55ed55ab72
Bas van den Wijngaard <bas@100.nl>
2013-04-22 12:26:42 +0200

    Updated demo to use jQuery 2.0.0 Updated docs

--- jquery.mfs.js
+++ jquery.mfs.js
@@ -4,7 +4,7 @@
  * Examples and documentation at: http://www.binkje.nl/mfs
  * 
  * Copyright (c) 2013 Bas van den Wijngaard
- * Version: 0.2.2
+ * Version: 0.2.3
  * Licensed under the MIT License:
  * http://www.binkje.nl/mfs/license
  *
